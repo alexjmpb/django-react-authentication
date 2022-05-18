054f00f18f44073b8bfc80767b059f8797143466
alexjmpb <alexjmpmendoza@gmail.com>
2022-05-17 21:04:49 -0500

    Django SimpleJWT config

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,4 +1,12 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
+)
+from django.urls import path
+
 
 from . import views as user_views
 
@@ -6,3 +14,10 @@ router = DefaultRouter()
 router.register(r'users', user_views.UserViewSet, basename='user')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('jwt/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist')
+]
